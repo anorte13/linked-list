78532af407982b579240e6767bc62041eb016dfc
Angel <angel-ortega27@hotmail.com>
2023-01-06 12:41:36 -0800

    updated classes to incude printSize and printHead methods

--- linked-list.js
+++ linked-list.js
@@ -1,58 +1,55 @@
 //the actual Node 'element' that we create using the LinkedList class
 class Node {
-    constructor(value = null, next = null){
-        this.value = value;
-        this.next= next;
-    }
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = next;
+  }
 }
 //the 'list' data structure which controls how nodes are added and placed
 class LinkedList {
-    constructor(head = null, size) {
-        this.head = null;
-        this.size = 0;
-    }
-//add element to end of the linked list
+  constructor(head = null) {
+    this.head = null;
+    this.size = 0;
+  }
+  //add element to end of the linked list
   append(value) {
-    const newNode = Node(value);
-    if (head === null) {
-        head = newNode;
+    const newNode = new Node(value);
+    if (this.head === null) {
+      this.head = newNode;
     } else {
-        let pointer = head;
-        while (pointer.next !== null){
-            pointer = pointer.next;
-        }
-        pointer.next = newNode;
+      let pointer = this.head;
+      while (pointer.next !== null) {
+        pointer = pointer.next;
+      }
+      pointer.next = newNode;
     }
-    size++;
+    this.size++;
   }
   //adds element to the start of the list
-  prepend(value){
-    const newNode = Node(value);
-    head = newNode;
-    size++;
+  prepend(value) {
+    this.head = new Node(value, this.head);
+    this.size++;
   }
-  size() {
-    console.log('The amount of elements in the list: ' + this.size);
+  // returns the size of the liked list
+  printSize() {
+    return console.log("The amount of elements in the list: " + this.size);
   }
-  head() {
-    console.log('The head of the linked list is: ' + this.head);
+  //returns the first node element in the list
+  printHead() {
+    return console.log("The head of the linked list is: " + this.head);
   }
-  tail() {
-    console.log('The last node in the list is: ' + Node.next);
+  printTail() {}
+  containsValue(value) {
+    if (head === null) {
+      return false;
+    } else {
+      while (this.head != value) {}
+    }
   }
-  at(index) {
-    
-  }
-  pop()
-  contains(valye){}
-  find(value){}
-  toString(){}
 }
-const list = LinkedList();
+const list = new LinkedList();
 list.append(5);
 list.append(6);
 list.prepend(10);
-list.size();
-
-  
-  
+list.printSize();
+list.printHead();
